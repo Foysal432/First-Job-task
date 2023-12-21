712d269f54f9d1096d4ce4f634a846942abf788e
foysal432 <foysalarham69@gmail.com>
2023-12-21 10:49:00 +0600

    added banner section

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom";
+
+const Banner = () => {
+    return(
+        <div>
+            <img src="https://i.ibb.co/C9XFLCH/White-Blue-Professional-Website-Developer-Linked-In-Banner.png" alt="Task Management" />
+            <NavLink><button className="btn -mt-20 ml-20 bg-yellow-300">Let’s Explore</button></NavLink>
+        </div>
+    )}
+export default Banner;
---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../Navbar";
+import Home from "../Page/Home";
+
+const Main = () => {
+    return(
+        <div>
+             <Navbar></Navbar>
+             <Home></Home>
+        </div>
+    )}
+export default Main;
